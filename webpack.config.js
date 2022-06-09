033e2a031137bf87a48bf573718aa68d67260ef6
baryska <barbora.skalova@newtontech.cz>
2022-06-09 10:37:00 +0200

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'public', to: '', noErrorOnMissing: true },
+        { from: 'public/**/*', to: '', noErrorOnMissing: true },
       ],
     }),
   ],
